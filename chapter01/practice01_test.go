8fdf67bf31d8b8ef94242cecdbffbc1500542f26
lirui <lirui@hangzhouyq.com>
2023-04-29 18:31:31 +0800

    test

---
+++ chapter01/practice01_test.go
@@ -0,0 +1,39 @@
+package chapter01
+
+import (
+	"fmt"
+	"sort"
+	"testing"
+)
+
+type User struct {
+	Uid      int
+	Name     string
+	Phone    string
+	Email    string
+	Password string
+}
+
+//func queryMultiRow() (User[]) {
+//	rows, err := db.Query("select uid,name,phone,email from `user` where uid > ?", 0)
+//	if err != nil {
+//		fmt.Printf("query err: %v", err)
+//	}
+//	defer rows.Close()
+//
+//}
+
+func prMain(t *testing.T) {
+	m := make(map[int]string)
+	m[0] = "I"
+	m[2] = "GO"
+	m[1] = "LOVE"
+	var keys []int
+	for k := range m {
+		keys = append(keys, k)
+	}
+	sort.Ints(keys)
+	for _, k := range keys {
+		fmt.Println("key:", k, "value:", m[k])
+	}
+}
