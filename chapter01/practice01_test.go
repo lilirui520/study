64b110e9d35e79a38a1334011240d2599c2760ed
miluo <miluo@dian.so>
2023-04-30 11:40:13 +0800

    提交

--- chapter01/practice01_test.go
+++ chapter01/practice01_test.go
@@ -1,8 +1,11 @@
 package chapter01
 
 import (
+	"database/sql"
+	"encoding/csv"
 	"fmt"
-	"sort"
+	"log"
+	"os"
 	"testing"
 )
 
@@ -14,31 +17,68 @@ type User struct {
 	Password string
 }
 
-func queryMultiRow() (User[]) {
+// 查询数据库并返回
+func queryMultiRow() []User {
+	dsn := "root:123456@tcp(127.0.0.1:3306)/test"
+	db, dberr := sql.Open("mysql", dsn)
+	if dberr != nil {
+		panic(dberr)
+	}
+
 	rows, err := db.Query("select uid,name,phone,email from `user` where uid > ?", 0)
 	if err != nil {
 		fmt.Printf("query err: %v", err)
 	}
 	defer rows.Close()
-	for _, v := range rows {
-		
+	//循环读取结果集合的数据
+	users := []User{}
+	//rows.Next()遍历结果集
+	for rows.Next() {
+		u := new(User)
+		//rows.Scan()将每一行的每一列保存到变量中。
+		err := rows.Scan(&u.Phone, &u.Email, &u.Password)
+		users = append(users, *u)
+		if err != nil {
+			fmt.Printf("scan failed,err %v \n", err)
+			return nil
+		}
 	}
-	append(User, )
-	rows.
+	return users
+}
 
+// 导出csv文件
+func ExportCSV(filepath string, data [][]string) {
+	fp, err := os.Create(filepath) // 创建文件句柄
+	if err != nil {
+		log.Fatalf("创建文件 %v", err)
+	}
+	defer fp.Close()
+	fp.WriteString("\xEF \xBB \xBF")
+	w := csv.NewWriter(fp)
+	//输入变量
+	w.WriteAll(data)
+	w.Flush()
 }
 
 func prMain(t *testing.T) {
-	m := make(map[int]string)
-	m[0] = "I"
-	m[2] = "GO"
-	m[1] = "LOVE"
-	var keys []int
-	for k := range m {
-		keys = append(keys, k)
+	//设置导入的文件名
+	filename := "./exportUsers.csv"
+	// 从数据库获取数据
+	users := queryMultiRow()
+	//定义二维数据标题
+	column := [][]string{{"手机", "邮箱", "密码"}}
+
+	for _, u := range users {
+		str := []string{}
+		str = append(str, u.Phone)
+		str = append(str, u.Email)
+		str = append(str, u.Password)
+		column = append(column, str)
 	}
-	sort.Ints(keys)
-	for _, k := range keys {
-		fmt.Println("key:", k, "value:", m[k])
-	}
+	//导出
+	ExportCSV(filename, column)
+}
+
+func queryMultiRows() {
+	panic("unimplemented")
 }
