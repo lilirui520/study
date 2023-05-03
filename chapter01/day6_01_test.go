7d6fe79b9c9180f21f70c687773d6e16b3ac80ae
lirui <lirui@hangzhouyq.com>
2023-05-03 20:04:46 +0800

    结构体封装

--- chapter01/day6_01_test.go
+++ chapter01/day6_01_test.go
@@ -21,6 +21,15 @@ type Response struct {
 	}
 }
 
+//圆形结构体
+type Circle struct {
+	Radius float32
+}
+
+func (t *Circle) Area() float32 {
+	return t.Radius * t.Radius * 3.14
+}
+
 func TestDay601Main(t *testing.T) {
 	jsonStr := `{"data": {
 	   "children" : [
@@ -29,4 +38,7 @@ func TestDay601Main(t *testing.T) {
 	//字符串转结构体
 	json.Unmarshal([]byte(jsonStr), &res)
 	fmt.Println(res.Data.Children)
+
+	r := Circle{10}
+	fmt.Println(r.Area())
 }
