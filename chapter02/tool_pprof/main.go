4a6e107dab28f01df9c370d103129f77b8f669a4
lirui <lirui@hangzhouyq.com>
2023-06-08 23:21:46 +0800

    pprof

---
+++ chapter02/tool_pprof/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+	"runtime"
+	"runtime/pprof"
+)
+
+var cpuprofile = flag.String("cpuprofile", "", "write cpu profile to `file`")
+var memprofile = flag.String("memprofile", "", "write memory profile to `file`")
+
+func main() {
+	flag.Parse()
+	if *cpuprofile != "" {
+		f, err := os.Create(*cpuprofile)
+		if err != nil {
+			log.Fatal("not create cpu profile:", err)
+		}
+		if err := pprof.StartCPUProfile(f); err != nil {
+			log.Fatal("not start cpu profile: ", err)
+		}
+		defer pprof.StopCPUProfile()
+	}
+	if *memprofile != "" {
+		f, err := os.Create(*memprofile)
+		if err != nil {
+			log.Fatal("could not create memory profile: ", err)
+		}
+		runtime.GC()
+		if err := pprof.WriteHeapProfile(f); err != nil {
+			log.Fatal("could not write memory profile: ", err)
+		}
+		f.Close()
+	}
+
+}
+
+//go run .\main.go --cpuprofile=cpu.prof
+//go tool pprof .\main.go .\cpu.prof
